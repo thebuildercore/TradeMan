ffe6ce95332cb9ddea5f15169daccff00a7a1c79
Akshaya <itz.akshaya001@gmail.com>
2025-06-21 11:02:00 +0530

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  }
+};
+
+export default config;
